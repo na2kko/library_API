e5ebb2bf10f418d0129c9ee3e84bd214439be61f
Diego <diego@netcommerce.mx>
2026-05-07 16:09:07 -0600

    Pipelines rework v.1.13

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -7,10 +7,10 @@ chown -R www-data:www-data storage bootstrap/cache
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-php artisan cache:clear
+
+php artisan migrate --force
 
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
 
 exec "$@"
